2b965f6e5b63a930530b4edebfca558211647af7
Emilio <Emilio@DESKTOP-VSU8A5A>
2024-07-01 19:04:03 +0200

    First Commit

---
+++ Sc-EliquidCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sc_EliquidCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
